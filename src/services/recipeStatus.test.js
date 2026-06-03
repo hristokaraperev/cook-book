27b4311d6fa2b35cf53ceb7a4cc9965fca003e36
Hristo Karaperev <hristokaraperev@gmail.com>
2026-06-04 01:41:50 +0300

    feature: bump Recipe Document Template version to formatted

--- src/services/recipeStatus.test.js
+++ src/services/recipeStatus.test.js
@@ -48,6 +48,24 @@ test('isIncompleteSave identifies recipes that cannot be shared yet', () => {
   )
 })
 
+test('the formatted template version marks plain-text era documents (version 1) as outdated', () => {
+  assert.equal(CURRENT_RECIPE_DOCUMENT_TEMPLATE_VERSION, 2)
+
+  const plainTextEra = {
+    driveDocUrl: 'https://docs.google.com/document/d/doc-1/edit',
+    documentTemplateVersion: 1,
+  }
+  assert.equal(hasCurrentGeneratedRecipeDocument(plainTextEra), false)
+  assert.equal(getRecipeDocumentStatus(plainTextEra).status, 'document-outdated')
+
+  const formattedEra = {
+    driveDocUrl: 'https://docs.google.com/document/d/doc-1/edit',
+    documentTemplateVersion: CURRENT_RECIPE_DOCUMENT_TEMPLATE_VERSION,
+  }
+  assert.equal(hasCurrentGeneratedRecipeDocument(formattedEra), true)
+  assert.equal(getRecipeDocumentStatus(formattedEra).status, 'ready')
+})
+
 test('getRecipeDocumentStatus explains the visible status for warning UI', () => {
   assert.deepEqual(getRecipeDocumentStatus({ driveDocUrl: null, documentTemplateVersion: 1 }), {
     status: 'incomplete-save',
